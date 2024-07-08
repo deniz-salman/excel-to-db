0d7e55d1802503437d0cdd15c9e5b8e8f30e952d
deniz-salman <denizsalman34@icloud.com>
2024-07-08 18:00:32 +0300

    chore: Update insert_data.sh script for improved SQL insertion and error handling

--- insert_data.sh
+++ insert_data.sh
@@ -1,1 +1,17 @@
-mysql -h 127.0.0.1 -u deniz -p delivery_db < "insert.sql"
+#!/bin/bash
+
+successful_inserts=0
+failed_inserts=0
+
+mysql -h 127.0.0.1 -u deniz -p'dnzpss' --force delivery_db < "insert.sql" 2>&1 | \
+while IFS= read -r line
+do
+    echo "$line" 
+    if [[ "$line" == *"Query OK"* ]]; then
+        successful_inserts=$((successful_inserts + 1))
+    elif [[ "$line" == *"ERROR"* ]]; then
+        failed_inserts=$((failed_inserts + 1))
+    fi
+    echo "Successful inserts: $successful_inserts"
+    echo "Failed inserts: $failed_inserts"
+done
