eeac87d7d4da87be52ba81a4e7401e0b875a4cc4
deniz-salman <denizsalman34@icloud.com>
2024-07-09 23:32:38 +0300

    chore: Update insert_data.sh script to increase max allowed packet size for SQL insertion

--- insert_data.sh
+++ insert_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysql -h 127.0.0.1 -u deniz -p'dnzpss' --force delivery_db < "insert.sql"
+mysql --max_allowed_packet=1073741824 -h 127.0.0.1 -u deniz -p'dnzpss' --force delivery_db < "insert.sql"
