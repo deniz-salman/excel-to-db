4c026450e9220aeb6cfcd4d5ca4562820aefa3e8
deniz-salman <denizsalman34@icloud.com>
2024-07-10 11:30:39 +0300

    chore: Optimize SQL insertion and file handling in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import sys
 
-batch_size_limit = 1000 * 1024 * 1024  
+batch_size_limit = 15 * 1024 * 1024  
 
 with open('headers_mapping.json', 'r') as f:
     header_mapping = json.load(f)
@@ -31,8 +31,12 @@ all_values = []
 table_name = "delivery"
 columns = None
 
+row_data_count = 0
+
 for excel_file in excel_files:
+    print(f"\rReading data from {excel_file}.", end='')
     df = pd.read_excel(excel_file, engine='openpyxl')
+    print(f"\rExtracting data from {excel_file}.")
 
     new_columns = {}
     for column in df.columns:
@@ -49,6 +53,8 @@ for excel_file in excel_files:
         columns = ", ".join([f"`{col}`" for col in df.columns])
 
     for _, row in df.iterrows():
+        row_data_count += 1
+        print(f"\rTotal rows processed: {row_data_count}", end='')
         values = ", ".join([escape_sql_value(val) for val in row.values])
         all_values.append(f"({values})")
 
@@ -56,13 +62,16 @@ for excel_file in excel_files:
             sql_statement = f"INSERT INTO {table_name} ({columns}) VALUES {', '.join(all_values)};"
             with open(insert_data_file, 'a') as f:
                 f.write(sql_statement + '\n')
+                print(f"\rAdded insert query to {insert_data_file} for {len(all_values)} rows.")
             all_values = []
 
-    print(f"Extracted data from {excel_file}.")
+    print(f"\rExtracted data from {excel_file}.")
 
 if all_values:
     sql_statement = f"INSERT INTO {table_name} ({columns}) VALUES {', '.join(all_values)};"
     with open(insert_data_file, 'a') as f:
         f.write(sql_statement + '\n')
+        print(f"Added insert query to {insert_data_file} for {len(all_values)} rows.")
 
+print(f"Total rows processed: {row_data_count}")
 print("SQL generation completed.")
